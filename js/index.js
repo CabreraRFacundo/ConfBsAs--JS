368d7e641fd0f92b58e958cec16703ecc3cb517b
Your Name <you@example.com>
2022-11-09 10:57:41 -0300

    Formato arreglado Y ahora solo acepta numeros

--- js/index.js
+++ js/index.js
@@ -7,10 +7,10 @@ let categoria;
 document.getElementById("boton").onclick = function () {
     var precioTicket = 200;
     var precioTotal = 0;
-    
+
     entradas = document.getElementById("cantidad_entradas").value;
     //console.log(entradas);
-    
+
     precioTicket = precioTicket * entradas;
     //console.log(precioTicket);
 
@@ -40,30 +40,3 @@ document.getElementById("restart").onclick = function () {
     document.getElementById("formulario").reset();
 };
 
-
-
-/*
-//precio de un ticket
-var precioTicket = 200;
-
-var precio = 0;
-var cantidad = 0;
-//total a pagar, pero sin descuento
-precio = precioTicket * cantidad;
-
-var descuento = 0; //descuento de estudiante, trainee o jr
-//total a pagar con el descuento aplicado
-var precioTotal = 0;
-
-//calculo del descuento para aplicar al precio total
-if (descuento === estudiante) {
-    precioTotal = (precio * 80) / 100;
-
-} else if (descuento === trainee) {
-    precioTotal = (precio * 50) / 100;
-
-} else { //jr
-    precioTotal = (precio * 15) / 100;
-}
-
-*/
